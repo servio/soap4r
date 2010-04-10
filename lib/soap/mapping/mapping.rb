24a30926899f5d2fd8992d61007c04d91cc10ab2
spox <spox@modspox.com>
2010-04-10 06:58:44 -0700

    1.9 updates. Thanks dlangevin

--- lib/soap/mapping/mapping.rb
+++ lib/soap/mapping/mapping.rb
@@ -540,7 +540,7 @@ module Mapping
   private
 
     def class_schema_variable(sym, klass)
-      var = "@@#{sym}"
+      var = "@@#{sym}".to_sym
       klass.class_variables.include?(var) ? klass.class_eval(var) : nil
     end
 
